55b3a10f41b948d68b74d01108e70fe15a0e6a09
Manda Raja Sekhar <RajaSekhar.Manda@cerner.com>
2022-11-24 17:04:49 +0530

    Initial commit

---
+++ src/test/java/com/mpk/samples/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpk.samples.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
